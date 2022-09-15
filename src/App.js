8dd16051f98067a8c4001d7ef14222f4d7eda5a7
Giovanni <giovag200@gmail.com>
2022-09-15 21:33:09 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+
+function App() {
+  return (
+    
+      <header className="App-header">
+      <nav>
+        <a href="#logo"> <img class="logo" src="https://cdn.shopify.com/s/files/1/0055/5525/7434/files/TEGA_Whtie.png?v=16483820442434495189" alt="Tega">
+          </img>
+        </a>
+        <ul>
+
+            <li><a href="#about">About us</a>
+            <a href="#products">Products</a>
+            <a href="#contact">Contact</a></li>npm start
+        </ul>
+
+
+ 
+    </nav>
+    <div class='home' >
+        <h1>TEGA SIMULATION</h1>
+    </div>
+  </header>
+  
+  );
+}
+
+export default App;
